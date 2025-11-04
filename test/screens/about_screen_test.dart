c531704055f2225900c699eeb40eab63db7fb1fd
NguyenDuyHieu49 <nguyenduyhieuwork49@gmail.com>
2025-11-04 17:30:31 +0700

    test(about): add widget test for about screen

---
+++ test/screens/about_screen_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:english_forum_app/screens/about_screen.dart';
+
+void main() {
+  testWidgets('AboutScreen shows correct title and message', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MaterialApp(home: AboutScreen()));
+
+    // Verify that the title is correct.
+    expect(find.text('About'), findsOneWidget);
+
+    // Verify that the message is displayed.
+    expect(find.text('This is an application for English communication and learning.'), findsOneWidget);
+  });
+}
